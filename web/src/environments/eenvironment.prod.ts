8b033e47710468d406add8ffd71298fa2777c102
Hicham El Alaoui El Hanafi <hicham.el-alaoui@mazars.fr>
2025-02-03 20:42:14 +0100

    refactoring styles

---
+++ web/src/environments/eenvironment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://your-production-url.com',  // Production Frontend URL
+  backendBaseUrl: 'https://your-backend-url.com', // Production Backend URL
+  database: 'mongodb+srv://your-production-db-url', // Production Database URL
+};
---
+++ web/src/environments/eenvironment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  baseUrl: 'http://localhost:4200',  // Local Frontend URL
+  backendBaseUrl: 'http://localhost:3000', // Local Backend URL
+  database: 'mongodb://localhost:27017/mydb',  // Local Database URL
+};
+//
